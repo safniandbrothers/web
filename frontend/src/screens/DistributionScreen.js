5365b73791674bff1053c2f0c856a8672f9dcd11
danushkan2002 <152047244+sdanushkan@users.noreply.github.com>
2024-05-26 09:32:54 +0530

    v1.3

--- frontend/src/screens/DistributionScreen.js
+++ frontend/src/screens/DistributionScreen.js
@@ -1,4 +1,4 @@
-import { Button, DateInput, Input } from '@nextui-org/react'
+import { Button, DateInput, Input, Textarea } from '@nextui-org/react'
 import { color, transform } from 'framer-motion';
 import React, { useState } from 'react'
 import { IoLocationSharp } from "react-icons/io5";
@@ -10,44 +10,138 @@ import dp from '../assets/dp.png'
 import { Link } from 'react-router-dom';
 import {Tabs, Tab, Card, CardBody, CardHeader} from "@nextui-org/react";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination, FreeMode, Autoplay } from 'swiper/modules';
+
 const DistributionScreen = () => {
   let [date, setDate] = useState(parseAbsoluteToLocal("2021-04-07T18:45:22Z"));
   return (
-    <div className='h-fit w-full'>
+    <div className='h-fit w-full flex flex-col gap-14 overflow-hidden'>
         <section className='h-fit w-full relative'>
-            <div className='h-[500px] w-full absolute z-0'>
+            <div className='h-[500px] w-full absolute z-0 opacity-35'>
                 <img src={dp} alt='' className='h-full w-full object-cover' />
             </div>
-            
-            <div className='h-[700px] w-full lg:w-1/2 max-w-[1024px] mx-auto flex flex-col gap-4 px-8 relative z-30 pt-[300px] '>
-              <p className='text-4xl font-bold text-[#4093f1]'>Who we are?</p>
-              <Tabs 
-                classNames={{
-                  tabList: "backdrop-blur-md",
-                  cursor: "",
-                  tab: "",
-                  tabContent: "group-data-[selected=true]:text-black"
-                }}
-                aria-label="Dynamic tabs" className='px-1'>
-                <Tab key={"services"} title={"Services"}>
-                  <div className='py-8 px-6 border-[2px] border-white/20 rounded-[24px] flex flex-col items-start gap-4 backdrop-blur-md'>
-                      <p className='text-xl font-bold text-white'>Our Services</p>
-                      <p className='text-white text-sm text-justify'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual.</p>
-                  </div>
-                </Tab>
-                <Tab key={"about"} title={'About'}>
-                  <div className='py-8 px-6 border-[2px] border-white/20 rounded-[24px] flex flex-col items-start gap-4 backdrop-blur-md'>
-                      <p className='text-xl font-bold text-white'>About us</p>
-                      <p className='text-white text-sm text-justify'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p>
-                  </div>
-                </Tab>
-              </Tabs>
-              
-              
+            <div className='h-fit w-full max-w-[1024px] mx-auto flex flex-col gap-4 px-8 relative z-30 items-start justify-center lg:justify-center pt-[400px]'>
+              <div className='h-fit w-full max-w-[600px] lg:mt-[100px] flex flex-col gap-2'>
+                <p className='text-6xl font-bold text-white'>Who we are?</p>
+                <p className='text-white/75 text-sm'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p> 
+              </div>      
             </div>
         </section>  
 
-        <section>
+        <section className='h-fit w-full relative'>
+          <div className='absolute bottom-0 h-[150px] w-full py-4 flex flex-col gap-4'>
+            <Swiper
+              slidesPerView={'4'}
+              spaceBetween={20}
+              freeMode={true}
+              loop={true}
+              centeredSlides={true}
+              modules={[FreeMode, Autoplay]}
+              autoplay={{
+                delay: 2000,
+            }}
+              className=' w-full'
+            >
+              <SwiperSlide className='h-[100px] w-fit flex items-center' >
+                <img src='https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F8ed3d547-94ff-48e1-9f20-8c14a7030a02_2000x2000.jpeg' alt='' className='h-full w-full object-contain' />
+              </SwiperSlide>
+              <SwiperSlide className='h-[100px] w-fit' >
+                <img src='https://images.ctfassets.net/y2ske730sjqp/5QQ9SVIdc1tmkqrtFnG9U1/de758bba0f65dcc1c6bc1f31f161003d/BrandAssets_Logos_02-NSymbol.jpg?w=940' alt='' className='h-full w-full object-contain' />
+              </SwiperSlide>
+              <SwiperSlide className='h-[100px] w-fit' >
+                <img src='https://i.pinimg.com/originals/74/79/a0/7479a0a02cf333e01e13d1b6d08af800.jpg' alt='' className='h-full w-full object-contain' />
+              </SwiperSlide>
+              <SwiperSlide className='h-[100px] w-fit' >
+                <img src='https://wallpapercave.com/wp/wp2860517.jpg' alt='' className='h-full w-full object-contain' />
+              </SwiperSlide>
+              <SwiperSlide className='h-[100px] w-fit' >
+                <img src='https://seekvectors.com/files/download/Amazon-Logo-31.jpg' alt='' className='h-full w-full object-contain' />
+              </SwiperSlide>
+              <SwiperSlide className='h-[100px] w-fit' >
+                <img src='https://i.pinimg.com/564x/0e/9b/1e/0e9b1e7319dae4b8046c5366d62532e3.jpg' alt='' className='h-full w-full object-contain' />
+              </SwiperSlide>
+
+            </Swiper>
+            
+          </div>
+          <div className='h-fit w-full max-w-[1024px] mx-auto px-8'>
+            <div className='flex flex-col gap-1'>
+              <p className='text-2xl text-white font-bold'>Our collaborators</p>
+              <p className='text-sm text-white/25'>Lorem Ipsum is simply dummy text,</p>
+            </div>
+            <div className='flex flex-col md:flex-row py-4 gap-4 h-[150px]'>
+            
+            </div>
+          </div>
+        </section> 
+
+        <section className='h-fit w-full'>
+          <div className='h-fit w-full max-w-[1024px] mx-auto px-8'>
+            <div className='flex flex-col gap-1'>
+              <p className='text-2xl text-white font-bold'>Do you want to Collaborate?</p>
+              <p className='text-sm text-white/50 font-semibold'>With us,</p>
+            </div>
+            <div className='flex flex-col md:flex-row py-4 gap-4'>
+              <div className='w-full lg:max-w-[400px] min-w-full lg:min-w-[400px] flex flex-col gap-4 px-6 py-8 bg-transparent backdrop-blur-md rounded-[16px] border-[2px] border-white/25'>
+                    <div className='flex items-start gap-4'>
+                        <Input variant="bordered" type="email" label="Email" className='text-white' style={{
+                                color:'white'
+                            }} placeholder="Enter your email" />
+                    </div>
+
+                    <div className='flex items-start gap-4'>
+                        <Input variant="bordered" type="text" label="Subject" placeholder="Enter Subject" />
+                    </div>
+
+                    <div className='flex items-start gap-4'>
+                      <Textarea
+                      variant='bordered'
+                        label="Description"
+                        placeholder="Enter your description"
+                        className="max-w-xs"
+                      />
+                    </div>
+
+                    <Link to={'/rent-car'}>
+                        <Button variant='bordered' className='h-fit w-fit capitalize border-[2px] font-medium border-[#4093f1] bg-[#4093f1] text-xs md:text-sm text-white px-5 py-3 rounded-full'>
+                            Submit
+                        </Button>
+                    </Link>
+                    
+                </div>
+            </div>
+          </div>
+        </section> 
+
+        <section className='h-fit w-full'>
+          <div className='h-fit w-full max-w-[1024px] mx-auto px-8'>
+            <div className='flex flex-col gap-1'>
+              <p className='text-2xl text-white font-bold'>Our Services</p>
+              <p className='text-sm text-white/25'>Lorem Ipsum is simply dummy text,</p>
+            </div>
+            <div className='flex flex-col md:flex-row py-4 gap-4'>
+              <div className='h-fit w-full py-4 px-4 border-[1px] gap-2 border-white/25 rounded-[12px] flex flex-col '>
+                <p className='text-lg font-medium text-white'>Service 1</p>
+                <p className='text-white/50 text-sm'>In publishing and graphic design, Lorem ipsum is a placeholder text</p>
+              </div>
+              <div className='h-fit w-full py-4 px-4 border-[1px] gap-2 border-white/25 rounded-[12px] flex flex-col '>
+                <p className='text-lg font-medium text-white'>Service 2</p>
+                <p className='text-white/50 text-sm'>In publishing and graphic design, Lorem ipsum is a placeholder text</p>
+              </div>
+              <div className='h-fit w-full py-4 px-4 border-[1px] gap-2 border-white/25 rounded-[12px] flex flex-col '>
+                <p className='text-lg font-medium text-white'>Service 3</p>
+                <p className='text-white/50 text-sm'>In publishing and graphic design, Lorem ipsum is a placeholder text</p>
+              </div>
+            </div>
+          </div>
         </section>      
     </div>
   )
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,53 +1,85 @@
 import React from 'react'
 import hbg from '../assets/HBG.png'
+import stikS from '../assets/stickS.png'
+import stikG from '../assets/stickG.png'
+
 import { Button } from '@nextui-org/react'
 import { Link } from 'react-router-dom'
-import { FaFacebook } from "react-icons/fa";
-import { IoLogoWhatsapp } from "react-icons/io";
+import { IoLogoFacebook, IoLogoWhatsapp } from "react-icons/io";
+import {Card, Skeleton} from "@nextui-org/react";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-flip';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { EffectFlip, Autoplay, Navigation } from 'swiper/modules';
+
 
 const HomeScreen = () => {
   return (
-    <div className='h-fit w-full flex flex-col gap-14 relative'>
-        <section className='h-fit w-full bg-black pt-[100px] '>
-            <img src={hbg} alt='' className='h-[600px] lg:h-[500px] w-full lg:w-[800px] object-cover absolute z-10 lg:right-0' />
-            <div className='h-[600px] lg:h-[500px] w-full max-w-[1024px] mx-auto flex lg:flex-row items-end lg:items-center lg:justify-start px-8 relative z-30'>
-                <div className='h-fit w-full lg:w-[400px] flex flex-col gap-4'>
-                    <div className='flex flex-col gap-2'>
-                        <div className='flex flex-col'>
-                            <p className='text-4xl font-bold text-white capitalize'>Welcome to</p>
-                            <p className='text-4xl font-bold text-[#4093f1] capitalize'>sufrian brothers</p>
-                        </div>
-                        <p className='text-sm text-white/80'>A simple declarative syntax means you write less code. Less code means your codebase is easier to read and maintain.</p>
+    <div className='h-fit w-full flex flex-col relative items-center justify-center'>
+        <section className='h-screen min-h-screen w-full bg-black relative'>
+            <div className='h-[500px] w-full absolute z-0 opacity-35'>
+                <img src={hbg} alt='' className='h-full w-full object-cover' />
+            </div>
+            <div className='h-full w-full flex flex-col gap-4 items-center justify-center  px-8 relative z-30'>
+                <div className='flex gap-2 justify-center lg:mt-8'>
+                    <Link to={'/destribution'} className='h-[200px] w-[200px] relative  rounded-full'>
+                    <Swiper
+                        effect={'flip'}
+                        grabCursor={true}
+                        autoplay={{
+                            delay: 2000,
+                            disableOnInteraction: false,
+                        }}
+                        modules={[EffectFlip, Autoplay]}
+                        
+                    >
+                        <SwiperSlide>
+                            <img src={stikG} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img src={stikS} />
+                        </SwiperSlide>
+                    </Swiper>
+                    </Link>
+                    {/* <Link to={'/rent-car'}>
+                        <Button variant='bordered' className='h-fit capitalize border-[2px] font-medium border-[#4093f1] bg-[#4093f1] text-xs md:text-sm text-black px-4 md:px-5 py-2 md:py-3 rounded-full'>
+                            Rent car
+                        </Button>
+                    </Link> */}
+                </div>
+                <div className='flex flex-col gap-2'>
+                    <div className='flex flex-col text-center'>
+                        <p className='text-xl font-semibold text-white capitalize'>Welcome to</p>
+                        <p className='text-3xl sm:text-4xl font-black text-[#4093f1] uppercase'>Safny & Brothers </p>
+                        <p className='text-sm font-black text-[#4093f1]'>(PVT) Ltd</p>
+                        
                     </div>
-                    <div className='flex gap-2'>
-                        <Link to={'/destribution'}>
-                            <Button variant='bordered' className='h-fit capitalize border-[2px] font-medium border-[#4093f1] text-xs md:text-sm text-[#4093f1] px-4 md:px-5 py-2 md:py-3 rounded-full'>
-                                destribution
-                            </Button>
-                        </Link>
-                        <Link to={'/rent-car'}>
-                            <Button variant='bordered' className='h-fit capitalize border-[2px] font-medium border-[#4093f1] bg-[#4093f1] text-xs md:text-sm text-black px-4 md:px-5 py-2 md:py-3 rounded-full'>
-                                Rent car
-                            </Button>
-                        </Link>
-                    </div>
+                    {/* <p className='text-sm text-white/50 text-center'>A simple declarative syntax means you write less code. Less code means your codebase is easier to read and maintain.</p> */}
+                </div>
+                <div className='flex gap-4'>
+                    <Button className='border-[2px] border-white/25' variant='bordered'>
+                        <IoLogoFacebook className='text-white text-2xl' />
+                        <p className='text-white capitalize'>facebook</p>
+                    </Button>
+                    <Button className='border-[2px] border-white/50' variant='bordered'>
+                        <IoLogoWhatsapp className='text-white text-2xl' />
+                        <p className='text-white capitalize'>WatsApp</p>
+                    </Button>
+                    
                 </div>
             </div>
         </section>
-        <section className='h-fit w-full'>
-            <div className='h-fit w-full max-w-[1024px] mx-auto px-8 flex flex-col gap-4 sm:grid sm:grid-cols-2 md:grid-cols-3'>
-                {/* <div className='py-8 px-6 border-[2px] border-white/20 rounded-[24px] flex items-center gap-4'>
-                    <FaFacebook className='text-white text-4xl' />
-                    <p className='text-lg font-bold text-white'>Rend a car </p>
-                </div>
-                <div className='py-8 px-6 border-[2px] border-white/20 rounded-[24px] flex items-center gap-4'>
-                    <FaFacebook className='text-white text-4xl' />
-                    <p className='text-lg font-bold text-white'>Destribution</p>
-                </div>
-                <div className='py-8 px-6 border-[2px] border-white/20 rounded-[24px] flex items-center gap-4'>
-                    <IoLogoWhatsapp className='text-white text-4xl' />
-                    <p className='text-lg font-bold text-white'>Rend a car </p>
-                </div> */}
+        <section className='h-fit w-full bg-gray-900 py-16'>
+            <div className='h-fit w-full max-w-[1024px] mx-auto px-8 flex flex-col sm:flex-row justify-center gap-4'>
+                
             </div>
         </section>
     </div>
